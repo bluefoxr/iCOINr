8200b9e5bb7d916c64777c952d1e498120edf22b
bluefoxr <william.becker@bluefoxdata.eu>
2023-05-21 22:01:58 +0200

    colouring for sunburst chart

--- R/plot_framework.R
+++ R/plot_framework.R
@@ -3,12 +3,11 @@
 #' Plots the structure of the index using a sunburst plot with **plotly**.
 #'
 #' @param coin The coin object
-#' @param seg_cols A character vector of colour codes, one for each segment in the plot. The length of this
-#' vector must be equal to the number of segments, i.e. the sum of the number of indicators and aggregates
-#' in each level.
+#' @param plotly_colorway Optional character vector of colour codes. This is passed to
+#' plotly's "colorway" parameter, and specifies the main colours to use in the chart.
 #'
 #' @export
-iplot_framework <- function(coin, seg_cols = NULL){
+iplot_framework <- function(coin, plotly_colorway = NULL){
 
   # get iMeta
   iMeta <- coin$Meta$Ind
@@ -20,41 +19,28 @@ iplot_framework <- function(coin, seg_cols = NULL){
     iMeta <- coin$Meta$Ind[!is.na(coin$Meta$Ind$Parent), ]
   }
 
-  #iMeta$EffWeight <- round(iMeta$EffWeight, 2)
+  fig <- plotly::plot_ly(
+    labels = iMeta$iCode,
+    parents = iMeta$Parent,
+    values = iMeta$EffWeight,
+    type = 'sunburst',
+    branchvalues = 'total',
+    text = iMeta$iName,
+    hoverinfo = 'text',
+    texttemplate = '%{label}'
+  )
 
-  if(is.null(seg_cols)){
-    fig <- plotly::plot_ly(
-      labels = iMeta$iCode,
-      parents = iMeta$Parent,
-      values = iMeta$EffWeight,
-      type = 'sunburst',
-      branchvalues = 'total',
-      text = iMeta$iName,
-      hoverinfo = 'text',
-      texttemplate = '%{label}'
+  if(!is.null(plotly_colorway)){
+
+    stopifnot(is.character(plotly_colorway))
+
+    fig <- plotly::layout(
+      fig,
+      colorway = plotly_colorway
     )
-  } else {
 
-    warning("This feature is not active yet...")
-    stopifnot(is.vector(seg_cols),
-              is.character(seg_cols))
-    # if(length(seg_cols) != length(outW$LabelsParents$Labels)){
-    #   stop("seg_cols is the wrong length: it needs to be a character vector of colour codes that is
-    #        the same length as the sum of all elements of the structure, in this case length should be ",
-    #        length(outW$LabelsParents$Labels))
-    # }
-    # fig <- plotly::plot_ly(
-    #   labels = iMeta$iCode,
-    #   parents = iMeta$Parent,
-    #   values = iMeta$EffWeight,
-    #   type = 'sunburst',
-    #   branchvalues = 'total',
-    #   marker = list(colors = seg_cols),
-    #   text = iMeta$iName,
-    #   hoverinfo = 'text',
-    #   texttemplate = '%{label}'
-    # )
   }
+
 
   fig
 }
